9e6106e91168b9a73ecccd0a5cfd561e53a94996
suhasj <suhasbjoshi@hotmail.com>
2014-01-06 17:31:44 -0800

    Added mapping files and db generation script

---
+++ NHibernate-Identity/CreateTable.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE [dbo].[AspNetRoles] (
+    [Id] [nvarchar](128) NOT NULL,
+    [Name] [nvarchar](max) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY ([Id])
+)
+CREATE TABLE [dbo].[AspNetUserRoles] (
+    [UserId] [nvarchar](128) NOT NULL,
+    [RoleId] [nvarchar](128) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId])
+)
+CREATE INDEX [IX_RoleId] ON [dbo].[AspNetUserRoles]([RoleId])
+CREATE INDEX [IX_UserId] ON [dbo].[AspNetUserRoles]([UserId])
+CREATE TABLE [dbo].[AspNetUsers] (
+    [Id] [nvarchar](128) NOT NULL,
+    [UserName] [nvarchar](max) NOT NULL,
+    [Email] [nvarchar](max),
+    [PasswordHash] [nvarchar](max),
+    [SecurityStamp] [nvarchar](max),
+    [IsConfirmed] [bit] NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY ([Id])
+)
+CREATE TABLE [dbo].[AspNetUserClaims] (
+    [Id] [int] NOT NULL IDENTITY,
+    [UserId] [nvarchar](128) NOT NULL,
+    [ClaimType] [nvarchar](max),
+    [ClaimValue] [nvarchar](max),
+    CONSTRAINT [PK_dbo.AspNetUserClaims] PRIMARY KEY ([Id])
+)
+CREATE INDEX [IX_UserId] ON [dbo].[AspNetUserClaims]([UserId])
+CREATE TABLE [dbo].[AspNetUserLogins] (
+    [UserId] [nvarchar](128) NOT NULL,
+    [LoginProvider] [nvarchar](128) NOT NULL,
+    [ProviderKey] [nvarchar](128) NOT NULL,
+    CONSTRAINT [PK_dbo.AspNetUserLogins] PRIMARY KEY ([UserId], [LoginProvider], [ProviderKey])
+)
+CREATE INDEX [IX_UserId] ON [dbo].[AspNetUserLogins]([UserId])
+
+
+ALTER TABLE [dbo].[AspNetUserRoles] ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[AspNetRoles] ([Id]) ON DELETE CASCADE
+ALTER TABLE [dbo].[AspNetUserRoles] ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+ALTER TABLE [dbo].[AspNetUserClaims] ADD CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+ALTER TABLE [dbo].[AspNetUserLogins] ADD CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
